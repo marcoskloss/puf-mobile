db79c9079df31dd1929e1aa62c195e7cb7bce87f
Marcos Kloss <marcoskloss2001@gmail.com>
2022-01-24 21:04:48 -0300

    feat: add theme definitions and provider

--- index.js
+++ index.js
@@ -1,9 +1,5 @@
-/**
- * @format
- */
-
 import { AppRegistry } from 'react-native'
-import { App } from './src'
 import { name as appName } from './app.json'
+import { Main } from './src'
 
-AppRegistry.registerComponent(appName, () => App)
+AppRegistry.registerComponent(appName, () => Main)
---
+++ src/screens/index.js
@@ -0,0 +1,14 @@
+import * as React from 'react'
+import { View, Text } from 'react-native'
+
+export const App = () => {
+    return (
+        <View>
+            <Text>Aloy!</Text>
+            <Text>Aloy!</Text>
+            <Text>Aloy!</Text>
+            <Text>Aloy!</Text>
+            <Text>Aloy!</Text>
+        </View>
+    )
+}
