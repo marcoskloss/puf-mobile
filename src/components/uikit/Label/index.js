d5e4177bf8511b3c5e887df39067aee1fd18a8a1
Marcos Kloss <marcoskloss2001@gmail.com>
2022-02-20 16:50:12 -0300

    feat: add Label component

---
+++ src/components/uikit/Label/index.js
@@ -0,0 +1,8 @@
+import styled from 'styled-components/native'
+
+import { Text } from '~/components/uikit/Text'
+import { th } from '~/components/Theme/helpers'
+
+export const Label = styled(Text)`
+    padding: ${th.space(2)}px ${th.space(3)}px;
+`
