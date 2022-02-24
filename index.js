6148b5d076fac0676fca755ff45f77da1099bb3e
Marcos Kloss <marcoskloss2001@gmail.com>
2022-02-24 20:39:43 -0300

    deps: add base64 and polyfill

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
 import { AppRegistry } from 'react-native'
+import { decode, encode } from 'base-64'
 import { name as appName } from './app.json'
+
 import { Main } from './src'
+
+if (!global.btoa) {
+    global.btoa = encode
+}
+
+if (!global.atob) {
+    global.atob = decode
+}
 
 AppRegistry.registerComponent(appName, () => Main)
